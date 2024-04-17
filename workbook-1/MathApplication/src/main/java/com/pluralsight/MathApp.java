815c69a3f311684473220f2e1f2f7d28879e20bd
alexis-renee <agrisom@pgh.yearup.org>
2024-04-16 21:34:08 -0400

    completed ShoppingList app

---
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args)
+    // find and display the largest of two varibles named job1Salary and job2Salary
+    double bobSalary = 13.13;
+    double garySalary = 26.04;
+    double highestSalary;
+    }
+
+    double radius = 7.25;
+    double areaOfCircle;
