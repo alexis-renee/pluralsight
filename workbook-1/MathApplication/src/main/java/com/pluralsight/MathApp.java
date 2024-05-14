7eaf0f1223d3e90da0f5cb8e395d663171211bc2
alexis-renee <agrisom@pgh.yearup.org>
2024-05-14 15:09:59 -0400

    updated constructors

--- workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -1,11 +1,13 @@
 package com.pluralsight;
 
 public class MathApp {
-    public static void main(String[] args)
-    // find and display the largest of two varibles named job1Salary and job2Salary
+    public static void main(String[] args);
+    // find and display the largest of two varables named job1Salary and job2Salary
     double bobSalary = 13.13;
     double garySalary = 26.04;
     double highestSalary;
+
+
     }
 
     double radius = 7.25;
