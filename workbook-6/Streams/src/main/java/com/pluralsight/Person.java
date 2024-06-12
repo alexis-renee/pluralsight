4a28f14ebf6174421fc9dbac544aa4434e57142b
alexis-renee <agrisom@pgh.yearup.org>
2024-06-11 22:30:28 -0400

    updated commit

---
+++ workbook-6/Streams/src/main/java/com/pluralsight/Person.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class Person {
+    private String FirstName;
+    private String LastName;
+    private int Age;
+
+    public Person(String firstName, String lastName, int age) {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+
+
+
+    }
+
